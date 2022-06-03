1838cd730ca8917d9b5fe9100511d14728bc55e3
Isaac <zacwrightson@gmail.com>
2022-06-03 23:52:59 +0100

    User can now perform custom search

--- main.py
+++ main.py
@@ -6,20 +6,44 @@ menu_options = {
     3: 'Exit',
 }
 
-def print_options():
-    for key in menu_options.keys():
-        print(key, '--', menu_options[key])
+find_menu_options = {
+    1: 'Find all instances of a word',
+    2: 'Find first instance of a word',
+    3: 'Custom search'
+}
+
+
+def print_options(chosen_menu):
+    for key in chosen_menu.keys():
+        print(key, '--', chosen_menu[key])
+
 
 def option1():
     sentencesplit.split_file()
 
+
 def option2():
-    wordsearch.search_word()
+    print("What kind of search would you like to perform?")
+    while True:
+        print_options(find_menu_options)
+        option = int(input("Enter your choice\n"))
+        if option == 1:
+            wordsearch.search_word(option)
+        if option == 2:
+            wordsearch.search_word(option)
+        if option == 3:
+            wordsearch.search_word(option)
+        elif option == 4:
+            print('Thank you for using LangIO!')
+            exit()
+        else:
+            print('Invalid option. Please enter a number between 1 and 4.')
+
 
 if __name__=='__main__':
     print("Hello, welcome to LangIO")
     while True:
-        print_options()
+        print_options(menu_options)
         option = int(input("Enter your choice\n"))
         if option == 1:
             option1()
@@ -29,4 +53,4 @@ if __name__=='__main__':
             print('Thank you for using LangIO!')
             exit()
         else:
-            print('Invalid option. Please enter a number between 1 and 4.')
+            print('Invalid option. Please enter a number between 1 and 3.')
--- wordsearch.py
+++ wordsearch.py
@@ -1,7 +1,48 @@
-def search_word():
+user_filter = {
+    "num_words": 0
+}
+
+
+def search_word(option):
     search_term = input("Input the word you would like to search for\n")
     fpath = input("Input the address of the file you would like to search")
     f = open(fpath, 'r', encoding='utf-8')
+    if option == 1:
+        sen_all = find_all(search_term, f)
+        for sen in sen_all:
+            print(sen)
+    elif option == 2:
+        print(first_sen(search_term, f))
+    elif option == 3:
+        sen_all = find_all(search_term, f)
+        min_words = input("What is the minimum length of your sentence?\n")
+        num_sen = input("How many sentences would you like?\n")
+        results = (custom_search(sen_all, min_words, num_sen))
+        for sen in results:
+            print(sen)
+
+
+def find_all(search_term, f):
+    sen_all = []
     for line in f:
-        if (search_term in line):
-            print(line)
+        if search_term in line:
+            sen_all.append(line)
+    return sen_all
+
+
+def first_sen(search_term, f):
+    for line in f:
+        if search_term in line:
+            return line
+
+
+def custom_search(sen_all, min_words, num_sen):
+    user_filter["num_words"] = min_words
+    results = []
+    for sen in sen_all:
+        word_list = sen.split()
+        if (len(word_list) > int(min_words)):
+            results.append(sen)
+        if len(results) == int(num_sen):
+            return results
+    return results
