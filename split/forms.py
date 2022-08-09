6c8813d01fced805bc54bb98a17f47f42aeb7fcf
Isaac <zacwrightson@gmail.com>
2022-08-10 00:11:17 +0100

    Split reddit thread comments and add comments to database

--- split/forms.py
+++ split/forms.py
@@ -1,6 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import FileField
+from wtforms import FileField, StringField, SubmitField
+from wtforms.validators import InputRequired
 
 
 class UploadForm(FlaskForm):
     file = FileField()
+
+
+class SearchForm(FlaskForm):
+    search_term = StringField(validators=[InputRequired()])
+    submit = SubmitField()
--- split/views.py
+++ split/views.py
@@ -1,18 +1,19 @@
 import re
 
 import nltk
-from flask import Blueprint, render_template, url_for
+import praw
+from flask import Blueprint, render_template, url_for, request
 from werkzeug.utils import secure_filename, redirect
 
 from main import session
 from models import Sentence
-from split.forms import UploadForm
+from split.forms import UploadForm, SearchForm
 
 # blueprint configuration
 split_bp = Blueprint('split', __name__, template_folder='templates')
 
 
-# main split page
+# main  file split page
 @split_bp.route('/split', methods=['GET', 'POST'])
 def split():
     form = UploadForm()
@@ -45,3 +46,25 @@ def split_file(finpath):
         sen = Sentence(sentence=s, word_count=word_count)
         session.add(sen)
         session.commit()
+
+# reddit search page
+@split_bp.route('/reddit', methods=['GET', 'POST'])
+def reddit():
+    form = SearchForm()
+
+    if request.method == "POST":
+        reddit = praw.Reddit(client_id='5034T2LHlpmOablUkj-vXg',
+                             client_secret='QPmtpn9fkhO5CbWwxosZJiH8pO1wbQ',
+                             user_agent='sentence scraper')
+
+        submission = reddit.submission(url=form.search_term.data)
+
+        for top_level_comment in submission.comments:
+            word_count = len(top_level_comment.body.split())
+            sen = Sentence(sentence=top_level_comment.body, word_count=word_count)
+            session.add(sen)
+            session.commit()
+
+        return render_template('reddit.html', form=form, comments = submission.comments)
+
+    return render_template('reddit.html', form=form)
